5ce61c55603cdbdb9c10442435f16bfa76af2fe1
Houston Culpepper <howacul@gmail.com>
2023-06-13 01:02:24 -0400

    updated schema.sql and added seeds

---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO department (name) 
+VALUES  ('Finance'),
+        ('Legal'),
+        ('Engineering'),
+        ('Sales');
+        
+INSERT INTO role (title, salary, department_id)
+VALUES  ('Sales Lead', 100000, 4),
+        ('Salesperson', 80000, 4),
+        ('Lead Engineer', 150000, 3),
+        ('Software Engineer', 120000, 3),
+        ('Account Manager', 160000, 1),
+        ('Accountant', 125000, 1),
+        ('Legal Team Lead', 250000, 2),
+        ('Lawyer', 190000, 2);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ('John', 'Doe', 1, null),
+        ('Mike', 'Chane', 2, 1),
+        ('Ashley', 'Rodriguez', 3, null),
+        ('Kevin', 'Tupik', 4, 3),
+        ('Kunal', 'Singh', 5, null),
+        ('Malia', 'Brown', 6, 5),
+        ('Sarah', 'Lourd', 7, null),
+        ('Tom', 'Allen', 8, 7);
+
+
+
